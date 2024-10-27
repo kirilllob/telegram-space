2834b5755a74a6b65ba3e3f9b94973b387d00eaa
kirilllob <146638785+kirilllob@users.noreply.github.com>
2024-10-27 15:39:15 +0300

    Update spacex.py

--- spacex.py
+++ spacex.py
@@ -1,9 +1,10 @@
 import requests
+import argparse
 from download import download_images
 
 
-def fetch_spacex_last_launch():
-    url = "https://api.spacexdata.com/v5/launches/5eb87d47ffd86e000604b38a"
+def fetch_spacex_last_launch(launch_id):
+    url = f"https://api.spacexdata.com/v5/launches/{launch_id}"
     response = requests.get(url)
     response.raise_for_status()
     url_images = response.json()["links"]["flickr"]["original"]
@@ -13,7 +14,10 @@ def fetch_spacex_last_launch():
 
 
 def main():
-    fetch_spacex_last_launch()
+    parser = argparse.ArgumentParser(description='этот скрипт позволяет скачивать фотографии spacex по указанному id запуска')
+    parser.add_argument('--launch_id', type=str, help='укажите id запуска', default='5eb87d47ffd86e000604b38a')
+    args = parser.parse_args()
+    fetch_spacex_last_launch(args.launch_id)
 
 
 if __name__ == '__main__':

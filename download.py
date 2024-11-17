4ca5533648d4cd5a1cd0cf909d02fb552833a309
kirilllob <146638785+kirilllob@users.noreply.github.com>
2024-11-17 15:38:46 +0300

    добавил аргумент

--- download.py
+++ download.py
@@ -2,9 +2,9 @@ import requests
 import os
 
 
-def download_image(url, filename, folder="images"):
+def download_image(url, filename, params=None, folder="images"):
     os.makedirs(folder, exist_ok=True)
-    response = requests.get(url)
+    response = requests.get(url, params=params)
     response.raise_for_status()
     with open(filename, 'wb') as file:
         file.write(response.content)

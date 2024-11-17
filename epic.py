5e4162ad14cdce87e69cf982d965a43619c3f055
kirilllob <146638785+kirilllob@users.noreply.github.com>
2024-11-17 15:40:22 +0300

    поменял аргументы местами

--- epic.py
+++ epic.py
@@ -14,7 +14,7 @@ def download_epic(api_key):
         date_image = datetime.fromisoformat(date_image).strftime("%Y/%m/%d")
         name_image = epic["image"]
         url_epic_images = f"https://api.nasa.gov/EPIC/archive/natural/{date_image}/png/{name_image}.png"
-        download_image(url_epic_images,payload, f"images/{name_image}.png")
+        download_image(url_epic_images, f"images/{name_image}.png", payload)
 
 
 def main():

f6333af76ffd6cec621bdf495a4ba26b68be922b
kirilllob <146638785+kirilllob@users.noreply.github.com>
2024-11-24 14:35:36 +0300

    добавил переменную

--- tgBot.py
+++ tgBot.py
@@ -7,15 +7,16 @@ from dotenv import load_dotenv
 
 def main():
     bot = telegram.Bot(token=os.environ["TG_TOKEN"])
-    Departure_time = "4200"
+    departure_time = "4200"
+    chat_id = os.environ["TG_CHAT_ID"]
     while True:
         pictures = os.listdir("images")
         random.shuffle(pictures)
         for picture in pictures:
             file_path = os.path.join("images", picture)
             with open(file_path, "rb") as file:
-                bot.send_photo(chat_id=os.environ["TG_CHAT_ID"], photo=file)
-            sleep(Departure_time)
+                bot.send_photo(chat_id=chat_id, photo=file)
+            sleep(departure_time)
 
 
 if __name__ == '__main__':
